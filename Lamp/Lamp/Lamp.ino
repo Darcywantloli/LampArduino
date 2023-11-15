dc108a050497aaeee6e7840accde4ec24eeb9ca5
Darcywantloli <darcygood01@gmail.com>
2023-11-15 15:53:24 +0800

    檯燈判斷修改，整理程式碼

--- Lamp/Lamp/Lamp.ino
+++ Lamp/Lamp/Lamp.ino
@@ -4,16 +4,16 @@
 SoftwareSerial HM10(2, 3);
 // 宣告WS2812變數(燈泡數, pin)
 Adafruit_NeoPixel pixels(16, 6);
-// 宣告變數 Vr(可變電阻)
-const byte Vr = A0;
-// 宣告變數 Bluetooth(藍牙狀態), LampOnOff(檯燈狀態)
-bool Bluetooth = false;
-bool LampOnOff = false;
+// 宣告變數 vr(可變電阻)
+const byte vr = A0;
+// 宣告變數 bluetoothOnOff(藍牙狀態), lampOnOff(檯燈狀態)
+bool bluetoothOnOff = false;
+bool lampOnOff = false;
 // 藍牙特徵值 
 char appData;
 String appString = "";
 // 亮度
-int LEDBright = 0;
+int ledBright = 0;
 
 void setup() {
   // 13：藍牙開關, 12：檯燈開關
@@ -28,54 +28,88 @@ void setup() {
   pixels.begin();
   pixels.clear();
 }
+
 void loop() {
   HM10.listen();
   HM10.setTimeout(100);
-
-  if (HM10.available()) {
-    appData = HM10.read();
-    appString = String(appData);
-    if (appString == "C") {
-      LampOnOff = true;
-      Bluetooth = true;
-      LEDBright = 255;
-      Serial.println("Off");
-    }
-  }
-
-  if (digitalRead(13) == LOW) {
-    Bluetooth = !Bluetooth;
-    Serial.println("Bluetooth,OK");
-    delay(1000);
-  }
   
-  if (Bluetooth == false) {
-    digitalWrite(7, LOW);
-    lamp();
-  } else {
-    digitalWrite(7, HIGH);
-    bluetooth();
-  }
-
-  if (LampOnOff == false) {
-    pixels.setBrightness(0);
-    pixels.show();
-  } else {
-    if (Bluetooth == false) {
-      int VrValue = analogRead(Vr);
-      VrValue = ((VrValue / 4) - 90) * 1.5;
-      setLight(VrValue);
-    } else {
-      setLight(LEDBright);
-    }
-  }
+  bluetooth();
+  lamp();
 }
 
 void lamp() {
   if (digitalRead(12) == LOW) {
-    LampOnOff = !LampOnOff;
-    Serial.println("Lamp,OK");
+    lampOnOff = !lampOnOff;
+    Serial.println("Lamp,switch");
     delay(1000);
+  }
+  if (lampOnOff == false) {
+    setLight(0);
+  } else {
+    if (bluetoothOnOff == false) {
+      int vrValue = analogRead(vr);
+      vrValue = ((vrValue / 4) - 90) * 1.5;
+      setLight(vrValue);
+    } else {
+      setLight(ledBright);
+    }
+  }
+}
+
+void bluetooth() {
+  if (digitalRead(13) == LOW) {
+    bluetoothOnOff = !bluetoothOnOff;
+    Serial.println("Bluetooth,switch");
+    delay(1000);
+  }
+  if (bluetoothOnOff == false) {
+    digitalWrite(7, LOW);
+  } else {
+    digitalWrite(7, HIGH);
+  }
+  if (HM10.available()) {
+    appData = HM10.read();
+    appString = String(appData);
+    if (appString == "A") {
+      lampOnOff = false;
+      ledBright = 0;
+      Serial.println("A");
+    }
+    if (appString == "B") {
+      lampOnOff = true;
+      ledBright = 255;
+      Serial.println("B");
+    }
+    if (appString == "C") {
+      bluetoothOnOff = true;
+      lampOnOff = true;
+      ledBright = 255;
+      Serial.println("C");
+    }
+    if (appString == "0") {
+      ledBright = 1;
+      Serial.println("0");
+    }
+    if (appString == "1") {
+      ledBright = 51;
+      Serial.println("1");
+    }
+    if (appString == "2") {
+      ledBright = 102;
+      Serial.println("2");
+    }
+    if (appString == "3") {
+      ledBright = 153;
+      Serial.println("3");
+    }
+    if (appString == "4") {
+      ledBright = 204;
+      Serial.println("4");
+    }
+    if (appString == "5") {
+      ledBright = 255;
+      Serial.println("5");
+    }
   }
 }
 
@@ -85,45 +119,4 @@ void setLight(int bright) {
   }
   pixels.setBrightness(bright);
   pixels.show();
-}
-
-void bluetooth() {
-  if (HM10.available()) {
-    appData = HM10.read();
-    appString = String(appData);
-    if (appString == "A") {
-      LampOnOff = false;
-      LEDBright = 0;
-      Serial.println("Off");
-    }
-    if (appString == "B") {
-      LampOnOff = true;
-      LEDBright = 255;
-      Serial.println("On");
-    }
-    if (appString == "0") {
-      LEDBright = 0;
-      Serial.println("0");
-    }
-    if (appString == "1") {
-      LEDBright = 51;
-      Serial.println("1");
-    }
-    if (appString == "2") {
-      LEDBright = 102;
-      Serial.println("2");
-    }
-    if (appString == "3") {
-      LEDBright = 153;
-      Serial.println("3");
-    }
-    if (appString == "4") {
-      LEDBright = 204;
-      Serial.println("4");
-    }
-    if (appString == "5") {
-      LEDBright = 255;
-      Serial.println("5");
-    }
-  }
 }
